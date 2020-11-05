2baa69e78caed0693986398e54f5a144b0fa4bcb
NerlyTon <nerlyton@gmail.com>
2020-11-05 14:37:31 -0600

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+let win = record.find(function(e) { return e.result === "W" })
+return win ? win.year : undefined
+}
+
+// record.find(function(e) { return e.result === "W" })
