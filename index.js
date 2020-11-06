bb991b6bfe1136d20bbb34e88ea653547e680f4c
NickSpangler <Spangleyman@mac.com>
2020-11-06 17:15:15 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let win = array.find(game => { return game['result'] === "W"})
+  if (win) {
+    return win['year']
+  }
+}
