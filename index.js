96ed19abd1b6503f7e3b956fd1433ae54914471e
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-11-06 16:52:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 const testVar = {}
 
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+]
+
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  debugger
+  let win = array.find(function (obj) {return obj.result === "W"})
+  if (win) {
+    return win.year
+  } else {
+    return win
+  }
+}
+
+// superbowlWin(record);
